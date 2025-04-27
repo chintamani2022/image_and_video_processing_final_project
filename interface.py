54640c80da8f7ec88efde41ba41fa6a1dd81d56d
chintamani2022 <chanud2020@gmail.com>
2025-04-27 19:25:42 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,5 +1,11 @@
-from model import DeepCNN
-from dataset import get_loaders
-from train import train_model
-from predict import classify_images
+#model import 
+from model import DeepCNN as TheModel
+#training import
+from train import train_model as the_trainer
+#predict function 
+from predict import classify_images as the_predictor
+##dataset class and dataloader content
+from dataset import CustomDataset as TheDataset #for dataset having class
+from dataset import get_loaders as the_dataloader 
+#config parameters
 from config import resize_x, resize_y, batch_size, num_epochs, learning_rate
